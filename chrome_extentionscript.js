f652056519c483e8528972c0add474462c920589
Sid-op777 <69779993+Sid-op777@users.noreply.github.com>
2022-10-30 23:43:11 +0530

    commit updated files

--- chrome_extentionscript.js
+++ chrome_extentionscript.js
@@ -1,7 +1,26 @@
 let myLeads = []
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
+
+if (leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    renderLeads(myLeads)
+}    
+
+tabBtn.addEventListener("click",function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads",JSON.stringify(myLeads))
+        renderLeads(myLeads)
+    })
+
+})
+
 
 inputBtn.addEventListener("click",function(){
     if (inputEl.value===""){
@@ -10,30 +29,34 @@ inputBtn.addEventListener("click",function(){
     else{
         
         myLeads.push(inputEl.value)
-        renderLeads()
+        localStorage.setItem("myLeads",JSON.stringify(myLeads))
+        renderLeads(myLeads)
         inputEl.value = ""
         inputEl.removeAttribute("placeholder")      
     }
 
 })
 
-
-function renderLeads(){
+function renderLeads(leads){
     let listItems = ""
-    for (i=0; i<myLeads.length;i++){
-        //listItems += "<li><a target = '_blank' href='"+ myLeads[i] +"'>" + myLeads[i] + "</a></li>"
+    for (i=0; i<leads.length;i++){
+        //listItems += "<li><a target = '_blank' href='"+ leads[i] +"'>" + myLeads[i] + "</a></li>"
         listItems += `
             <li>
-                <a target = '_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target = '_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
         //const li = document.createElement("li")
-        //li.textContent = myLeads[i]
+        //li.textContent = leads[i]
         //ulEl.append(li)
     }
     ulEl.innerHTML = listItems
 }
 
-
+deleteBtn.addEventListener("dblclick",function(){
+    localStorage.clear()
+    myLeads = []
+    renderLeads(myLeads)
+})
